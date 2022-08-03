54c3731074fbb5ead5aa2c1fd95ad02c97577d08
manoloacademia <luispablosegovia@icloud.com>
2022-08-03 20:06:34 -0300

    Se añaden al admin los modelos creados.

--- vinos/base/admin.py
+++ vinos/base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from models import Wine, Cellar, Brand
 
 # Superuser created manoloacademia: 1234*5678
+
+admin.site.register(Wine)
+admin.site.register(Cellar)
+admin.site.register(Brand)
