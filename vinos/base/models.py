587e1f6d55dd7516536487506f51890c10f1f684
manoloacademia <luispablosegovia@icloud.com>
2022-08-03 20:03:39 -0300

    Modelos añadidos a la base de datos

--- vinos/base/models.py
+++ vinos/base/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Wine(models.Model):
+    name = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50)
+    price = models.FloatField()
+    aged = models.IntegerField()
+    cellar = models.CharField(max_length=50)
+
+class Cellar(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
+
+class Brand(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
