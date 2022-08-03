b3c31db3b874c48596c6d4ca009964897824b9c3
manoloacademia <luispablosegovia@icloud.com>
2022-08-03 19:54:19 -0300

    Se crea funcionalidad de lista de vinos.

--- vinos/base/views.py
+++ vinos/base/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, 'home.html', context=context)
+
+def wine_list(request):
+    context = {}
+    return render(request, 'wine_list.html', context=context)
--- vinos/vinos/urls.py
+++ vinos/vinos/urls.py
@@ -20,4 +20,5 @@ from base import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('lista-vinos/', views.wine_list, name="wine_list"),
 ]
